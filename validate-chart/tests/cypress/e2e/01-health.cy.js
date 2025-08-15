227555f6212a313ce32d49d0d21e7fc3802cd20a
Zach Callahan <zach@zmc.dev>
2025-08-15 13:41:50 -0500

    Resolve "Add netpol for gluon's pod"

--- validate-chart/tests/cypress/e2e/01-health.cy.js
+++ validate-chart/tests/cypress/e2e/01-health.cy.js
@@ -1,6 +1,9 @@
-describe('Dummy Cypress Test', () => {
-    it('Dummy Cypress Test', () => {
-      cy.wait(1000)
-      cy.log("Dummy Cypress Test")
-    })
-  })
+describe("Cypress", () => {
+  it("waits 10 seconds for network policies to be in effect", () => {
+    cy.wait(10000);
+  });
+
+  it("visits the cypress example page", () => {
+    cy.visit("https://example.cypress.io");
+  });
+});
